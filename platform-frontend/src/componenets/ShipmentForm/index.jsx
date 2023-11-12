95d55f06568b9a9e8efdfd82a6d5032c6ca399c3
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-11-12 17:22:06 +0200

    Frontend <> added image field

--- platform-frontend/src/componenets/ShipmentForm/index.jsx
+++ platform-frontend/src/componenets/ShipmentForm/index.jsx
@@ -5,28 +5,41 @@ function ShipmentForm() {
 
     const [name, setName] = useState('');
     const [error, setError] = useState('');
+    const [image, setImage] = useState(null);
     const [address, setAddress] = useState('');
     const [number, setNumber] = useState('');
     const [waybill, setWaybill] = useState('');
 
-
     const handleShipmentCreation = async (event) => {
         event.preventDefault();
 
-        if (!name || !waybill) {
+        if (!name || !waybill || !image || !address || !number) {
             setError('All fields are required');
             return;
         }
 
         try {
             createShipment(waybill, address, name, number);
+
             setName("");
+            setImage(null);
             setNumber("");
             setAddress("");
             setWaybill("");
 
         } catch (error) {
             console.error('failed:', error);
+        }
+    };
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setImage(reader.result.split(',')[1]);
+            };
+            reader.readAsDataURL(file);
         }
     };
 
@@ -56,6 +69,11 @@ function ShipmentForm() {
                 <div className="label-input">
                     <label htmlFor="number">Number </label>
                     <input required id="number" name="number" type="text" placeholder="phone number" value={number} onChange={(e) => setNumber(e.target.value)} />
+                </div>
+
+                <div className="label-input">
+                    <label htmlFor="image">Image </label>
+                    <input required id="image" name="image" type="file" onChange={handleImageChange} />
                 </div>
 
                 {error && <p className="error-message">{error}</p>}
