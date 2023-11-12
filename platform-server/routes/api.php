bd62253c44f5e0ea730197c105848dfab3029ba3
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-11-12 18:06:41 +0200

    Server <> created createShipment route

--- platform-server/routes/api.php
+++ platform-server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ShipmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,12 +9,12 @@ Route::group(['prefix' => 'guest'], function () {
 
     Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
     Route::post('register', [AuthController::class, 'register']);
-
     Route::post('login', [AuthController::class, 'login']);
 });
 
 Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
 
+    Route::post("/shipment/create", [ShipmentController::class, "createShipment"]);
     Route::post("logout", [AuthController::class, "logout"]);
     Route::post("refresh", [AuthController::class, "refresh"]);
 });
