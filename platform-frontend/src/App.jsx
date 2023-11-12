4523b17d9c170758f27d533f252c15e6cac7b3a8
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-11-12 16:21:58 +0200

    Frontend <> created the shipments page

--- platform-frontend/src/App.jsx
+++ platform-frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Authentication from "./pages/Authentication";
+import Shipments from "./pages/Shipments";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path="" element={<Authentication/>} />
+      <Routes>
+        <Route path="" element={<Authentication />} />
 
-        <Route path="/user/*" element={<Layout/>}>
+        <Route path="/user/*" element={<Layout />}>
 
-          {/* <Route index element={<Home />} />
+          <Route index element={<Shipments />} />
           <Route path="create" element={<Create />} />
-          <Route path="calendar" element={<Calendar />} />
-          <Route path="favourites" element={<Favourite />} /> */}
 
         </Route>
       </Routes>
---
+++ platform-frontend/src/pages/Shipments/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Shipments() {
+
+    return (
+        <div className="Shipments">
+        </div>
+    );
+}
+
+export default Shipments
